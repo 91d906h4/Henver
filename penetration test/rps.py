3a3824494e707924384cf035ffad66fc9579367f
91d906h4 <fairymanning@gmail.com>
2023-10-08 17:11:58 +0900

    update

---
+++ penetration test/rps.py
@@ -0,0 +1,16 @@
+# Send n requests persecond to Henver.
+# 
+# The max RPS (Requests Per Second) of Henver HTTP Server is about 1000 
+# with Intel Code i7 CPU.
+
+import threading
+import requests
+
+def test():
+    for _ in range(100):
+        requests.request("GET", "http://127.0.0.1/temp/test.php?test=222")
+
+for i in range(1000):
+    threading.Thread(target=test).start()
+
+print("done")
