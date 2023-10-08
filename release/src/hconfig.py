b1d9809d3f6410d48b7eefa0572f9cd490400717
91d906h4 <fairymanning@gmail.com>
2023-10-08 17:25:59 +0900

    update

---
+++ release/src/hconfig.py
@@ -0,0 +1,39 @@
+# Import modules
+import configparser
+
+# Read conf file.
+main_conf = configparser.ConfigParser()
+main_conf.read("./config/main.ini")
+
+# Const settings.
+# [Server]
+SERVER_HOST     = str(main_conf["server"]["host"])
+SERVER_PORT     = int(main_conf["server"]["port"])
+PHP_CGI         = str(main_conf["server"]["php_cgi"])
+DEFAULT_PAGE    = str(main_conf["server"]["default_page"])
+DEFAULT_PAGE    = str(main_conf["server"]["default_page"])
+E400_PAGE       = str(main_conf["server"]["e400_page"])
+E403_PAGE       = str(main_conf["server"]["e403_page"])
+E404_PAGE       = str(main_conf["server"]["e404_page"])
+E409_PAGE       = str(main_conf["server"]["e409_page"])
+E414_PAGE       = str(main_conf["server"]["e414_page"])
+E500_PAGE       = str(main_conf["server"]["e500_page"])
+E502_PAGE       = str(main_conf["server"]["e502_page"])
+E503_PAGE       = str(main_conf["server"]["e503_page"])
+APPEND_SER_NAME = str(main_conf["server"]["append_ser_name"])
+
+# [Sys]
+ENCODING        = str(main_conf["sys"]["encoding"])
+LOGGING         = str(main_conf["sys"]["logging"])
+LOG_PATH        = str(main_conf["sys"]["log_path"])
+LOG_LEVEL       = str(main_conf["sys"]["log_level"]).upper()
+
+# [Security]
+ALLOWED_METHODS = str(main_conf["security"]["allowed_methods"]).upper()
+DIR_TRAV_FILTER = str(main_conf["security"]["dir_trav_filter"])
+CLEAR_URL_PENC  = str(main_conf["security"]["clear_url_penc"])
+URL_ONLY_ALNUM  = str(main_conf["security"]["url_only_alnum"])
+PREVENT_DDOS    = str(main_conf["security"]["prevent_ddos"])
+DDOS_COUNT      = str(main_conf["security"]["ddos_count"])
+DDOS_TIME       = str(main_conf["security"]["ddos_time"])
+MAX_URL_LEN     = str(main_conf["security"]["max_url_len"])
---
+++ release/src/hsecurity.py
@@ -0,0 +1,101 @@
+# hsecurity
+# 
+#   This module will analysis and filter out illegal requests.
+
+# Import modules.
+import json
+
+from src.logger import logger
+from urllib.parse import unquote
+from src.hconfig import ALLOWED_METHODS, DIR_TRAV_FILTER, CLEAR_URL_PENC, URL_ONLY_ALNUM, \
+                        PREVENT_DDOS, MAX_URL_LEN, DDOS_COUNT, DDOS_TIME
+
+# Get banned IP address list.
+f = open("./config/ban.json")
+ban = json.load(f)
+f.close()
+
+def hsecurity(request: list, clinet_ip: str="") -> dict:
+    method          = request["method"]
+    file            = request["file"]
+    file_type       = request["file_type"]
+    file_extension  = request["file_extension"]
+    query_string    = request["query_string"]
+    host            = request["host"]
+    user_agent      = request["user_agent"]
+    server_errors   = request["server_errors"]
+
+    # Detect the requests that is too long.
+    if MAX_URL_LEN == "-1": pass
+    elif len(file) > int(MAX_URL_LEN): server_errors = 414
+
+    # Clear URL percent-encoding characters.
+    # 
+    # This section will remove all space and decode percent-encoded characters.
+    # Some URL Encoded Attacks will use something like "%2520" to avoid detection ("%2520" is 
+    # the percent-encoding of "%20", and "%20" is the encoding of space), so we keep decoding
+    # the URL untill there is no more encoded characters in it.
+    if CLEAR_URL_PENC == "enable":
+        while "%" in file:
+            file = unquote(file)
+            file = file.replace(" ", "")
+
+    # Filter out banned IP address.
+    # All requests from banned IP will be dropped.
+    if clinet_ip in ban["ban_ip"]:
+        logger("WARN", f"Access from banned IP address {clinet_ip}.", clinet_ip)
+        exit(-1)
+
+    # Filter out illegal HTTP methods.
+    # All requests with illegal methods will be dropped.
+    if ALLOWED_METHODS == "all": pass
+    elif method not in ALLOWED_METHODS:
+        logger("WARN", f"Get illegal HTTP method {method}.", clinet_ip)
+        exit(-1)
+
+    # Remove all illegal characters in URL.
+    if URL_ONLY_ALNUM == "enable":
+        clear_file = ""
+
+        for char in file:
+            # Using "set" to make sure the search time compelixty is O(1).
+            if char in {
+                # 0-9.
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", \
+
+                # a-z.
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", \
+                "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", \
+                "u", "v", "w", "x", "y", "z", \
+
+                # A-Z.
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", \
+                "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", \
+                "U", "V", "W", "X", "Y", "Z", \
+
+                # Special characters.
+                "_", "?", "&", "#", "/", "=", "-", ":", ".", "+", \
+                "(", ")"
+            }: clear_file += char
+
+        file = clear_file
+
+    # Detect DOS/DDOS attack.
+    if PREVENT_DDOS == "enable":
+        pass
+
+    # Redirect (fix) the queries that tend to do directory traversal.
+    if DIR_TRAV_FILTER == "enable":
+        while ".." in file: file = file.replace("..", "")
+        while "//" in file: file = file.replace("//", "")
+
+    return {
+        "method":           method,
+        "file":             file,
+        "file_type":        file_type,
+        "file_extension":   file_extension,
+        "query_string":     query_string,
+        "host":             host,
+        "user_agent":       user_agent,
+        "server_errors":    server_errors,
+    }
